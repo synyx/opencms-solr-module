8029ffbdcb101473f623d8a4a30b22b2a15d7146
Florian Hopf <hopf@synyx.de>
2011-04-29 13:50:33 +0200

    new configuration bean for index utrl, refs #488

---
+++ src/main/java/org/synyx/opencms/solr/IndexConfiguration.java
@@ -0,0 +1,35 @@
+package org.synyx.opencms.solr;
+
+import java.net.MalformedURLException;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.BinaryRequestWriter;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+
+/**
+ * Bean that contains any index configuration options.
+ * @author Florian Hopf, Synyx GmbH & Co. KG, hopf@synyx.de
+ */
+public class IndexConfiguration {
+
+    private final String url;
+
+    public IndexConfiguration(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Initializes the SolrServer that is used for the index.
+     * @return
+     * @throws IllegalStateException in case the server can't be created
+     */
+    public SolrServer initializeServer() {
+        
+        try {
+            CommonsHttpSolrServer httpSolrServer = new CommonsHttpSolrServer(url);
+            httpSolrServer.setRequestWriter(new BinaryRequestWriter());
+            return httpSolrServer;
+        } catch (MalformedURLException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+}
